32862d57b9d617cbea53d61501e5481de2ab5dd4
christophrus <christophrus@protonmail.com>
2026-01-21 15:43:23 +0100

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/kita-betreuungsschluessel/sw.js', { scope: '/kita-betreuungsschluessel/' })})}
